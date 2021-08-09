4f16ab1054b2eb764252e8f274ea0932bf5129b1
MichaelYin1994 <zhuoyin94@163.com>
2021-08-10 00:00:17 +0800

    (feat) 表情分析：添加了简单convnet，resnet疑似过拟合

--- iflytek-human-face-emotion/input_pipeline.py
+++ iflytek-human-face-emotion/input_pipeline.py
@@ -25,12 +25,13 @@ from tensorflow.keras import Model
 from tensorflow.keras import backend as K
 from tensorflow.keras import layers
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.utils import to_categorical
 from tqdm import tqdm
 
 from dingtalk_remote_monitor import RemoteMonitorDingTalk
 from utils import LearningRateWarmUpCosineDecayScheduler, LoadSave
 
-GLOBAL_RANDOM_SEED = 7555
+GLOBAL_RANDOM_SEED = 1256
 # np.random.seed(GLOBAL_RANDOM_SEED)
 # tf.random.set_seed(GLOBAL_RANDOM_SEED)
 
@@ -52,6 +53,67 @@ if gpus:
         print(e)
 
 # ----------------------------------------------------------------------------
+def build_simple_convnet(verbose=False, is_compile=True, **kwargs):
+    '''构造preprocessing与model的pipeline，并返回编译过的模型。'''
+
+    # 解析preprocessing与model的参数
+    # ---------------------
+    input_shape = kwargs.pop('input_shape', (None, 224, 224))
+    n_classes = kwargs.pop('n_classes', 1000)
+
+    model_name = kwargs.pop('model_name', 'EfficentNetB0')
+    model_lr = kwargs.pop('model_lr', 0.01)
+    model_label_smoothing = kwargs.pop('model_label_smoothing', 0.1)
+
+    # 构建模型
+    # ---------------------
+    layer_input = tf.keras.layers.Input(shape=input_shape, dtype='float32')
+
+    layer_input = tf.keras.layers.BatchNormalization()(layer_input)
+    layer_conv = tf.keras.layers.Conv2D(
+        filters=256, kernel_size=5, padding='same',
+    )(layer_input)
+
+    layer_conv = tf.keras.layers.BatchNormalization()(layer_conv)
+    layer_conv = tf.keras.layers.Conv2D(
+        filters=512, kernel_size=3, padding='same',
+    )(layer_conv)
+
+    layer_conv = tf.keras.layers.BatchNormalization()(layer_conv)
+    layer_conv = tf.keras.layers.Conv2D(
+        filters=256, kernel_size=3, padding='same',
+    )(layer_conv)
+
+    layer_avg_pool = tf.keras.layers.GlobalAveragePooling2D()(layer_conv)
+    layer_max_pool = tf.keras.layers.GlobalMaxPool2D()(layer_conv)
+
+    layer_avg_pool_flatten = tf.keras.layers.Flatten()(layer_avg_pool)
+    layer_max_pool_flatten = tf.keras.layers.Flatten()(layer_max_pool)
+
+    layer_output = tf.keras.layers.concatenate(
+        [layer_avg_pool_flatten, layer_max_pool_flatten]
+    )
+    layer_output = tf.keras.layers.Dropout(0.3)(layer_output)
+    layer_output = tf.keras.layers.Dense(
+        n_classes, activation='softmax'
+    )(layer_output)
+
+    # 编译模型
+    # ---------------------
+    model = tf.keras.Model(layer_input, layer_output)
+
+    if verbose:
+        model.summary()
+
+    if is_compile:
+        model.compile(
+            loss=tf.keras.losses.CategoricalCrossentropy(
+                label_smoothing=model_label_smoothing),
+            optimizer=Adam(model_lr),
+            metrics=['acc'])
+
+    return model
+    
 
 def build_resnetv2_model(verbose=False, is_compile=True, **kwargs):
     '''构造preprocessing与model的pipeline，并返回编译过的模型。'''
@@ -76,15 +138,14 @@ def build_resnetv2_model(verbose=False, is_compile=True, **kwargs):
     elif '152' in model_name:
         model_tmp = tf.keras.applications.ResNet152V2
 
+    # 构建ResNet模型
     model_tmp = model_tmp(
-            input_shape=input_shape, 
-            include_top=False,
-            weights='imagenet',
-        )
-    model_tmp.trainable = False
-    model.add(
-        model_tmp
+        input_shape=input_shape, 
+        include_top=False,
+        weights='imagenet',
     )
+    model.add(model_tmp)
+
     model.add(tf.keras.layers.GlobalAveragePooling2D())
     model.add(tf.keras.layers.Flatten())
     model.add(tf.keras.layers.Dense(
@@ -180,18 +241,16 @@ def load_preprocessing_img(image_size, stage):
     if stage == 'train' or stage == 'test':
         def load_img(path=None):
             image = tf.io.read_file(path)
-            image = tf.cond(
-                tf.image.is_jpeg(image),
-                lambda: tf.image.decode_jpeg(image, channels=3),
-                lambda: tf.image.decode_gif(image)[0])
+            image = tf.image.decode_jpeg(image, channels=1)
+            # image = tf.concat([image, image, image], axis=-1)
 
-            image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
-            image = tf.image.random_hue(image, max_delta=0.2)
-            image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
-            image = tf.image.random_brightness(image, 0.3)
+            # image = tf.image.random_saturation(image, lower=0.5, upper=1.5)
+            # image = tf.image.random_hue(image, max_delta=0.2)
+            # image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
+            # image = tf.image.random_brightness(image, 0.3)
 
-            image = tf.image.random_flip_left_right(image)
-            image = tf.image.random_flip_up_down(image)
+            # image = tf.image.random_flip_left_right(image)
+            # image = tf.image.random_flip_up_down(image)
 
             image = tf.image.resize(image, image_size)
             image = tf.keras.layers.experimental.preprocessing.Rescaling(1. / 255.)(image)
@@ -199,12 +258,10 @@ def load_preprocessing_img(image_size, stage):
     else:
         def load_img(path=None):
             image = tf.io.read_file(path)
-            image = tf.cond(
-                tf.image.is_jpeg(image),
-                lambda: tf.image.decode_jpeg(image, channels=3),
-                lambda: tf.image.decode_gif(image)[0])
+            image = tf.image.decode_jpeg(image, channels=1)
+            image = tf.concat([image, image, image], axis=-1)
 
-            image = tf.image.resize(image, image_size)
+            # image = tf.image.resize(image, image_size)
             image = tf.keras.layers.experimental.preprocessing.Rescaling(1. / 255.)(image)
             return image
 
@@ -214,14 +271,14 @@ def load_preprocessing_img(image_size, stage):
 if __name__ == '__main__':
     # 全局化的参数列表
     # ---------------------
-    IMAGE_SIZE = (128, 128)
+    IMAGE_SIZE = (48, 48)
     BATCH_SIZE = 128
     NUM_EPOCHS = 256
     EARLY_STOP_ROUNDS = 30
-    N_FOLDS = 4
+    N_FOLDS = 5
     TTA_ROUNDS = 5
-    IS_STRATIFIED = False
-    MODEL_NAME = 'ResNet50v2_rtx3090'
+    IS_STRATIFIED = True
+    MODEL_NAME = 'ResNet50V2_rtx3090'
 
     MODEL_LR = 0.0003
     MODEL_LABEL_SMOOTHING = 0
@@ -269,16 +326,21 @@ if __name__ == '__main__':
         6: 'surprised',
     }
     train_label_oht_array = [label2id[item] for item in train_label_list]
-    train_label_oht_array = np.array(train_label_list)
+    train_label_oht_array = np.array(train_label_oht_array)
 
     test_file_name_list = os.listdir(TEST_PATH)
     test_file_fullname_list = \
         [os.path.join(TEST_PATH, item) for item in test_file_name_list]
 
     # 编码训练标签
-    encoder = OneHotEncoder(sparse=False)
-    train_label_oht_array = encoder.fit_transform(
-        train_label_oht_array.reshape(-1, 1)).astype(np.float32)
+    train_label_oht_array = to_categorical(train_label_oht_array.reshape(-1, 1))
+
+    # shuffle类间样本
+    shuffled_idx = np.arange(0, len(train_label_oht_array))
+    np.random.shuffle(shuffled_idx)
+    train_file_full_name_list = [train_file_full_name_list[i] for i in shuffled_idx]
+    train_label_list = [train_label_list[i] for i in shuffled_idx]
+    train_label_oht_array = train_label_oht_array[shuffled_idx, :]
 
     # 准备train数据与test数据的loader
     processor_train_image = load_preprocessing_img(
@@ -363,11 +425,24 @@ if __name__ == '__main__':
         train_ds = train_ds.batch(BATCH_SIZE).prefetch(16 * BATCH_SIZE)
         val_ds = val_ds.batch(BATCH_SIZE).prefetch(16 * BATCH_SIZE)
 
+        # 随机可视化几张图片
+        # ************
+        if IS_RANDOM_VISUALIZING_PLOTS:
+            plt.close('all')
+            plt.figure(figsize=(10, 10))
+            for images, labels in train_ds.take(1):
+                for i in range(9):
+                    ax = plt.subplot(3, 3, i + 1)
+                    plt.imshow(images[i].numpy().astype('uint8'))
+                    plt.title(str(labels[i].numpy()), fontsize=9)
+                    plt.axis('off')
+            plt.tight_layout()
+
         # 构造与编译模型
         # ***********
-        model = build_resnetv2_model(
+        model = build_simple_convnet(
             n_classes=N_CLASSES,
-            input_shape=IMAGE_SIZE + (3,),
+            input_shape=IMAGE_SIZE + (1,),
             network_type=MODEL_NAME,
             model_name=MODEL_NAME,
             model_lr=MODEL_LR,
